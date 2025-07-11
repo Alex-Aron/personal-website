fa8ed760ed64e9962ae7ce550b52b1b1e3a64045
Alex-Aron <alex.aron789123@gmail.com>
2025-07-11 10:35:27 -0400

    upload images

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import index from "./index.html";
 const server = serve({
   routes: {
     // Serve static files from public directory
-    "/headshot.jpg": () => new Response(Bun.file("src/images/headshot.jpg")),
-    "/Alex_s.pdf": () => new Response(Bun.file("src/images/Alex_s.pdf")),
+    "/headshot.jpg": () => new Response(Bun.file("public/headshot.jpg")),
+    "/Alex_s.pdf": () => new Response(Bun.file("public/Alex_s.pdf")),
     
     // Serve index.html for all unmatched routes (SPA routing)
     "/*": index,
